ab4fe2299345c1e73be33dd70df195ce2bb500a5
Sloia <kalipo777@gmail.com>
2026-06-11 14:31:54 +0400

    fix(host): fixed return type mismatch

--- host/cpu_monitor.py
+++ host/cpu_monitor.py
@@ -2,7 +2,7 @@ import psutil
 
 
 def get_cpu(interval=1.0) -> float:
-    return psutil.cpu_percent(interval=interval)
+    return float(psutil.cpu_percent(interval=interval))
 
 
 def format_msg(cpu: float) -> str:
